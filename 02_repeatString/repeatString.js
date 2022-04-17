dac8bbb4c4c79e8ed3d375a1d5b73bf4a2be38ef
x0Byte <cybersecuritystudent@protonmail.com>
2022-04-17 19:17:53 +0100

    complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  if (number < 0) {
+    return "ERROR";
+  } else {
+    return string.repeat(number);
+  }
 };
+
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+repeatString("hey");
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
