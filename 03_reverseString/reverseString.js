fa318c4a7b40eaed2127084d367cc0eef0c07ceb
x0Byte <cybersecuritystudent@protonmail.com>
2022-04-20 21:59:22 +0100

    passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = (text) => {
+  // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+  let reversedString = text.split("").reverse().join("");
 
+  return reversedString;
 };
 
 // Do not edit below this line
