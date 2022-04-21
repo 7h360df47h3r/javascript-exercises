12a65508159ccb68fdac0e754b8b89d61de7d42c
x0Byte <cybersecuritystudent@protonmail.com>
2022-04-21 08:55:15 +0100

    passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (first = 1, second = 4) {
+  //Implement a function that takes 2 integers and ...
+  //returns the sum of every number between(and including) them:
 
+  //https://www.w3schools.com/Jsref/jsref_sign.asp
+  if (first < 0 || second < 0) {
+    return "ERROR";
+  } else if (typeof first !== "number" || typeof second !== "number") {
+    return "ERROR";
+  } else {
+    let result =
+      ((Math.max(first, second) - Math.min(first, second) + 1) *
+        (Math.min(first, second) + Math.max(first, second))) /
+      2;
+
+    return result;
+  }
 };
 
 // Do not edit below this line
