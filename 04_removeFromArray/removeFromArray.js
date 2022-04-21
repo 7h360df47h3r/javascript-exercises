9b926d7929f54f10d7712b55fa85d7bd293cb269
x0Byte <cybersecuritystudent@protonmail.com>
2022-04-21 08:20:47 +0100

    pased

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = (array, ...exclude) => {
+  let bucket = array;
 
+  exclude.forEach((currentValue) => {
+    if (bucket.includes(currentValue)) {
+      bucket.splice(bucket.indexOf(currentValue), 1);
+      console.log(`${currentValue} removed from array`);
+    } else {
+      console.log(`${currentValue} does not exist within ${bucket}`);
+    }
+  });
+
+  return bucket;
 };
+
+//removeFromArray([1, 2, 3, 4, 5], 4, 5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
