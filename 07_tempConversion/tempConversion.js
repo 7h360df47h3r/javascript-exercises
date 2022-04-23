ee32d62c86fa4bfb1c61498f61025e65e3da69bc
x0Byte <cybersecuritystudent@protonmail.com>
2022-04-23 17:00:28 +0100

    passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
+const ftoc = (temp) => {
+  temp = ((temp - 32) * 5) / 9;
+  rounded = parseFloat(temp.toFixed(1));
 
+  if (temp == 0) return temp;
+  else return rounded;
 };
 
-const ctof = function() {
+const ctof = (temp) => {
+  temp = (temp * 9) / 5 + 32;
+  rounded = parseFloat(temp.toFixed(1));
 
+  if (temp == 0) return temp;
+  else return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
